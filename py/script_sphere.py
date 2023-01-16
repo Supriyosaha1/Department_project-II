ca213f6f66c317270d14677dd4e3caa3278290d8
Leo Michel-Dansac <leo.michel-dansac@univ-lyon1.fr>
2023-01-16 16:08:34 +0100

    update python example script_sphere.py

--- py/script_sphere.py
+++ py/script_sphere.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3.10
 
 # imports 
 import argparse
 import sys
 from matplotlib import pyplot as plt
 import numpy as np
-import photons as p
+import jphot as pp
+import lya_utils as lya
 
 
 # command-line arguments
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-f","--file", help="file to read (default is ./photons_done.dat)",default="./photons_done.dat")
+parser.add_argument("-fres","--fileResults", help="Results file to read (default is ./photons_done.dat)",default="./photons_done.dat")
+
+parser.add_argument("-fics","--fileICs", help="ICs file to read (default is ./ppic.dat)",default="./ppic.dat")
 
 parser.add_argument("-temp","--temperature", type=float, help="temparature of the gas (default 1.e4 K)",default="1.e4")
 
@@ -24,10 +27,11 @@ parser.add_argument("-nbin","--nbin",type=int,help="number of bins for histogram
 #    sys.exit(1)
 args = parser.parse_args()
 
-filePhotons = args.file
+fileRes = args.fileResults
+fileICs = args.fileICs
 nbin = args.nbin
 
-grid = p.from_file(filePhotons)
+p = pp.photonlist(fileICs,fileRes)
 
 # Dijkstra 2006, formulae C-17 
 # analytic solution from Dijkstra+06
@@ -35,13 +39,14 @@ grid = p.from_file(filePhotons)
 temperature = args.temperature
 tau_0       = args.tau
 
-print "nu_0 [Hz] =",p.nu_0
-print "c [cm/s]  =",p.clight
-print "T         =",temperature
-print "tau_0     =",tau_0
-print "nbin      =",nbin
+print("nu_0 [Hz] =",lya.nu0)
+print("c [cm/s]  =",lya.clight)
+print("T         =",temperature)
+print("tau_0     =",tau_0)
+print("nbin      =",nbin)
 
-x = np.arange(-100.,100.,0.01)
+xlim = 100.
+x = np.arange(-xlim,xlim,0.01)
     
 vth_kms = 12.9 * np.sqrt(temperature / 1.e4) # km/s
 vth_cgs = vth_kms * 1.e5 # cm/s
@@ -52,13 +57,10 @@ J_x     = x**2 * np.sqrt(np.pi) / (np.sqrt(24.)*a*tau_0) / (1+np.cosh(np.sqrt(2.
 # normalisation of this is 2*PI
 # in Dijkstra2014 norm = 4.*PI => factor 2
 
+delta_nu = lya.nu0 * vth_cgs / lya.clight
+xx = (p.nu - lya.nu0) / delta_nu
 
-delta_nu = p.nu_0 * vth_cgs / p.clight
-
-xx = (grid.nu - p.nu_0) / delta_nu
-
-
-hh, xb = np.histogram(xx,bins=nbin,normed=True)
+hh, xb = np.histogram(xx,bins=nbin,density=True,range=(-xlim,xlim))
 
 plt.step(xb[:-1],hh/4/np.pi,where='post')
 ##plt.plot(xb[:-1],hh/4/np.pi)
